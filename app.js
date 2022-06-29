b32aadea29699da81e9d3cd9aebd83c43556a2c4
rajivranjan96 <107671699+rajivranjan96@users.noreply.github.com>
2022-06-29 16:14:07 +0530

    Edit option enable for todo list

--- app.js
+++ app.js
@@ -2,31 +2,50 @@
 const todoInput = document.querySelector('.todo-input');
 const todoBtn = document.querySelector('.todo-btn');
 const todoList = document.querySelector('.todo-list');
-let msg = document.getElementById("msg");
 
 //Event Listeners
 todoBtn.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteTodo);
+// todoList.addEventListener('click', editBtn);
 
-function addTodo(event){
+function addTodo(event)
+
+{
     //prevent the form from submitting
     event.preventDefault();
 
     //add todo only when user enters something
     if(todoInput.value !== ''){
+        
         //create wrapper div
         const todoDiv = document.createElement('div');
         todoDiv.className = 'todo';
 
         //create list element
-        const newTodo = document.createElement('li');
+        const newTodo = document.createElement('input');
         newTodo.className = 'todo-item';
-        newTodo.innerText = todoInput.value;
+        newTodo.value = todoInput.value;
+
+        //my work
+
+        // const newTodo1 = document.createElement('input');
+        // newTodo1.className = 'todo-item1';
+        // newTodo1.value = newTodo.innerText;
+
+        // document.addEventListener("onclick", myFunction);
+
+        //     function myFunction() {
+        //         document.document.querySelector("todo-item1").value;
+        //     }
+
+
 
         //add that list element inside of wrapper div
         todoDiv.appendChild(newTodo);
         todoInput.value = '';
 
+        
+    
         //create complete task button
         const completedBtn = document.createElement('button');
         completedBtn.innerText = `✔️`;
@@ -39,12 +58,22 @@ function addTodo(event){
         trashBtn.classList.add('trash-btn');
         todoDiv.appendChild(trashBtn);
 
+        //create edit button
+        
+        // const editBtn = document.createElement('button');
+        // editBtn.innerText = `Edit`;
+        // editBtn.classList.add('edit-btn');
+        // todoDiv.appendChild(editBtn);
+
+
         //Add wrapper div to ul
         todoList.appendChild(todoDiv);
     }
 }
 
-function deleteTodo(e){
+function deleteTodo(e)
+
+{
     const item = e.target;
 
     if(item.classList[0] === 'trash-btn'){
@@ -63,3 +92,9 @@ function deleteTodo(e){
 
     }
 }
+// function editBtn(){
+//     document.getElementsByClassName('todo-item').innerText= newtodo1.value;
+// }
+
+
+
